d2530f2f6bc45545c369ed9c61e2efedb73d6190
deenu143 <deenbandhunetam488@gmail.com>
2024-03-03 03:01:44 +0500

    update

--- app/src/main/java/com/tyron/code/language/kotlin/KotlinLanguage.java
+++ app/src/main/java/com/tyron/code/language/kotlin/KotlinLanguage.java
@@ -3,10 +3,16 @@ package com.tyron.code.language.kotlin;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+import com.tyron.builder.BuildModule;
+import com.tyron.builder.project.Project;
+import com.tyron.builder.project.api.AndroidModule;
+import com.tyron.builder.project.api.Module;
 import com.tyron.code.ApplicationLoader;
 import com.tyron.code.language.CompletionItemWrapper;
+import com.tyron.code.ui.project.ProjectManager;
+import com.tyron.code.util.TaskExecutor;
 import com.tyron.completion.model.CompletionItem;
-import com.tyron.completion.model.CompletionList;
+import com.tyron.completion.model.DrawableKind;
 import com.tyron.editor.Editor;
 import io.github.rosemoe.sora.lang.Language;
 import io.github.rosemoe.sora.lang.analysis.AnalyzeManager;
@@ -20,6 +26,16 @@ import io.github.rosemoe.sora.text.ContentReference;
 import io.github.rosemoe.sora.text.TextUtils;
 import io.github.rosemoe.sora.util.MyCharacter;
 import io.github.rosemoe.sora.widget.SymbolPairMatch;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
 
@@ -58,15 +74,73 @@ public class KotlinLanguage implements Language {
       return;
     }
     String prefix = CompletionHelper.computePrefix(content, position, this::isAutoCompleteChar);
-    KotlinAutoCompleteProvider provider = new KotlinAutoCompleteProvider(mEditor);
+
+  /*  CompletableFuture<String> future =
+        TaskExecutor.executeAsyncProvideError(
+            () -> {
+              Project currentProject = ProjectManager.getInstance().getCurrentProject();
+              if (currentProject != null) {
+                Module module = currentProject.getModule(mEditor.getCurrentFile());
+                if (module instanceof AndroidModule) {
+                  File libraries = new File(module.getBuildDirectory(), "libraries");
+                  List<String> jars = listFiles(libraries.toPath(), ".jar");
+                  jars.add(BuildModule.getAndroidJar().getAbsolutePath());
+                  jars.add(BuildModule.getLambdaStubs().getAbsolutePath());
+
+                  jars.forEach(
+                      jar -> {
+                        try {
+                          ZipFile zipFile = new ZipFile(jar);
+                          Enumeration entries = zipFile.entries();
+
+                          while (entries.hasMoreElements()) {
+                            ZipEntry zipEntry = (ZipEntry) entries.nextElement();
+                            String filePath = zipEntry.getName();
+
+                            if (filePath.endsWith(".class")) {
+
+                              File classFile = new File(filePath);
+                              String classFileName = classFile.getName().replace(".class", "");
+
+                              String className = filePath.replace("/", ".");
+
+                              if (classFileName.startsWith(prefix)) {
+
+                                publisher.addItem(
+                                    new CompletionItemWrapper(
+                                        CompletionItem.create(
+                                            classFileName,
+                                            className,
+                                            classFileName,
+                                            DrawableKind.Class)));
+                              }
+                            }
+                          }
+                          zipFile.close();
+                        } catch (Exception e) {
+
+                        }
+                      });
+                }
+              }
+
+              return null;
+            },
+            (result, throwable) -> {});*/
+
+    //  publisher.addItem(new CompletionItemWrapper(new CompletionItem (prefix + " " +
+    // position.getLine() + position.getColumn())));
+
+      KotlinAutoCompleteProvider provider = new KotlinAutoCompleteProvider(mEditor);
     CompletionList list =
         provider.getCompletionList(prefix, position.getLine(), position.getColumn());
     if (list != null) {
       for (CompletionItem item : list.items) {
         CompletionItemWrapper wrapper = new CompletionItemWrapper(item);
         publisher.addItem(wrapper);
-      }
+              }
     }
+
   }
 
   public boolean isAutoCompleteChar(char p1) {
@@ -144,5 +218,13 @@ public class KotlinLanguage implements Language {
       int shiftLeft = text.length() + 1;
       return new NewlineHandleResult(sb, shiftLeft);
     }
+  }
+
+  private List<String> listFiles(Path directory, String extension) throws IOException {
+    return Files.walk(directory)
+        .filter(Files::isRegularFile)
+        .filter(path -> path.toString().endsWith(extension))
+        .map(Path::toString)
+        .collect(Collectors.toList());
   }
 }
