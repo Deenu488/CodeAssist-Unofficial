7a5ccf7c45b8a5137b6ea983aa1c03ae587a85da
trindadedev13 <devsuay@gmail.com>
2024-09-02 03:19:41 -0300

    fix: set new toolbar as supportActionBar

--- app/src/main/java/com/tyron/code/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/tyron/code/ui/settings/SettingsActivity.java
@@ -19,6 +19,7 @@ public class SettingsActivity extends AppCompatActivity
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.settings_activity);
+    setSupportActionBar(findViewById(R.id.toolbar));
     if (savedInstanceState == null) {
       getSupportFragmentManager()
           .beginTransaction()
